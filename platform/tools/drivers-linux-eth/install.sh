4be82ddc510c56224e8bd7d491b58a8c7a380459
Shannon Nelson <44790778+emusln@users.noreply.github.com>
2020-05-27 20:22:25 -0700

    linux: add install and remove scripts for distribution package (#24608)
    * linux: add install and remove scripts for distribution package
    
    Signed-off-by: Shannon Nelson <snelson@pensando.io>
    
    * linux_penctl: add penctl wrapper
    
    Add a shell script wrapper to make penctl.linux a little easier
    for customers to use.
    
    Signed-off-by: Shannon Nelson <snelson@pensando.io>

---
+++ platform/tools/drivers-linux-eth/install.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+#
+#  install.sh
+#
+#  Install Pensando DSC RPM and penctl utility from
+#  distribution tar file into host system.
+#
+#
+
+PROG=$0
+BASE_DIR=`pwd`
+
+
+#
+# check for expected directories
+#
+if [ ! -d bin -o ! -d drivers ] ; then
+	echo "$PROG: Error: file directories missing"
+	exit 1
+fi
+
+#
+# install penctl utility
+#
+PENCTL_FILES="penctl penctl.linux"
+PENCTL_DIR=/usr/local/bin/
+
+cd ${BASE_DIR}/bin
+for f in $PENCTL_FILES ; do
+	if [ ! -e $f ] ; then
+		echo "$PROG: Error: missing file $f"
+		exit 1
+	fi
+done
+
+install -D -p -t $PENCTL_DIR $PENCTL_FILES
+
+
+#
+# install RPM
+#
+
+if [ -f /etc/redhat-release ] ; then
+	ver=$(cat /etc/redhat-release | tr -dc '0-9')
+	major=${ver:0:1}
+	minor=${ver:1:4}
+
+	distro="rhel${major}u${minor}"
+	rpm="kmod-ionic"
+elif [ -f /etc/os-release ] ; then
+	ver=$(cat /etc/os-release | grep VERSION_ID | awk -F= '{print $2}' | tr -d '\"' | sed 's/\./sp/')
+
+	distro="sles${ver}"
+	rpm="ionic-kmp-default"
+else
+	echo "$PROG: no RPM available for this host, perhaps use drivers-linux-eth.tar.xz?"
+	exit 1
+fi
+
+cd ${BASE_DIR}/drivers/linux
+RPMFILE=`ls ${rpm}-*.${distro}.x86_64.rpm`
+
+rpm -ihv $RPMFILE
+
---
+++ platform/tools/drivers-linux-eth/remove.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+#  remove.sh
+#
+#  Remove Pensando DSC RPM and penctl utility from host system.
+#
+#
+
+PROG=$0
+BASE_DIR=`pwd`
+
+
+#
+# remove penctl utility
+#
+PENCTL_FILES="penctl penctl.linux"
+PENCTL_DIR=/usr/local/bin/
+
+cd ${PENCTL_DIR}
+rm -r $PENCTL_FILES
+
+
+#
+# remove RPM
+#
+
+if [ -f /etc/redhat-release ] ; then
+	rpm="kmod-ionic"
+elif [ -f /etc/os-release ] ; then
+	rpm="ionic-kmp-default"
+else
+	echo "$PROG: no RPM available for this host, perhaps use drivers-linux-eth.tar.xz?"
+	exit 1
+fi
+
+rpm -e $rpm
+
