1d5f8c23cffed6d1705cbb63bb432eeb5bd129e1
stavrospen <38233196+stavrospen@users.noreply.github.com>
2020-06-02 12:19:30 -0700

    Console: disable for unified mode (#24832)

--- nic/sysmgr/console/main.cc
+++ nic/sysmgr/console/main.cc
@@ -7,50 +7,10 @@
 #include <boost/property_tree/json_parser.hpp>
 #include <boost/property_tree/ptree.hpp>
 
-#define APP_START "/sysconfig/config0/app-start.conf"
-#define DEPRECATED_CONSOLE_MAGIC_FILE "/sysconfig/config0/.console"
+#define CONSOLE_MAGIC_FILE "/sysconfig/config0/.console"
 #define SYSTEM_CONFIG_FILE "/sysconfig/config0/system-config.json"
 
-#define HOSTPIN    "hostpin"
-#define HOSTPIN_SZ sizeof(HOSTPIN)
-
-#define CLASSIC_MODE 0
-#define HOSTPIN_MODE 1
-
 #define BUFLEN 32
-
-// Returns HOSTPIN_MODE if we are in hostpin mode. CLASSICE_MODE if not
-// We are in HOSTPIN_MODE *only* if APP_START file exists and it's contents are
-// "hostpin"
-static int nic_mode()
-{
-    int  fd;
-    char buffer[BUFLEN] = {0};
-    int  n;
-
-    fd = open(APP_START, O_RDONLY);
-    if (fd == -1)
-    {
-        return CLASSIC_MODE;
-    }
-
-    n = read(fd, buffer, BUFLEN);
-    close(fd);
-    
-    if (n == -1)
-    {
-        return CLASSIC_MODE;
-    }
-
-    // We don't care about the trailing \0 as long as what we read starts
-    // with 'hostpin' we are good
-    if (strncmp(HOSTPIN, buffer, HOSTPIN_SZ - 1) == 0)
-    {
-        return HOSTPIN_MODE;
-    }
-
-    return CLASSIC_MODE;
-}
 
 void
 wait_enter (void)
@@ -73,26 +33,13 @@ print_string (const char *str)
     fflush(NULL);
 }
 
-
-/*
- * Reads config settings file and returns:
- * TRUE if console is set to be enabled with no authenticaion need i.e.
- * "console": "enabled" is set in config settings json file.
- * FALSE otherwise.
- */
-static bool console_enabled (void)
+static bool console_disabled (void)
 {
     boost::property_tree::ptree config_spec;
     std::string console;
 
-    //Check if old way of .console magic file is present.
-    if (access(DEPRECATED_CONSOLE_MAGIC_FILE, F_OK) != -1) {
-        return (true);
-    }
-
     if (access(SYSTEM_CONFIG_FILE, F_OK) != -1)
     {
-        //Parse the config spec file.
         try {
             boost::property_tree::read_json(SYSTEM_CONFIG_FILE,
                                             config_spec);
@@ -101,14 +48,21 @@ static bool console_enabled (void)
                    ex.what());
             return (false);
         }
-        //Read and apply console preferences.
         console = config_spec.get("console", "None");
-        if (!strcasecmp(console.c_str(), "enable")) {
+        if (strcasecmp(console.c_str(), "disabled") == 0) {
             return (true);
         }
     }
-    // If no config file or the property setting doesn't compare to
-    // any expected value(s).
+    return (false);
+}
+
+static bool
+console_no_password (void)
+{
+    if (access(CONSOLE_MAGIC_FILE, F_OK) != -1) {
+        return (true);
+    }
+
     return (false);
 }
 
@@ -118,23 +72,18 @@ main (int argc, char *argv[])
     const char *auth[] = {"/bin/login", NULL};
     const char *no_auth[] = {"/bin/login", "-f", "root", NULL};
 
-    openlog("console", (LOG_CONS | LOG_PID | LOG_NDELAY), LOG_AUTH);
-    if (console_enabled()) {
+    if (console_no_password()) {
         // Set to drop straight into bash shell, with no authentication
         // need. Just do so.
         execvp(no_auth[0], (char* const*)no_auth);
     }
 
-    // Handle login per the NIC mode setting.
-    if (nic_mode() == HOSTPIN_MODE)
-    {
-        // In network managed mode. Disable console
+    if (console_disabled()) {
         while (1) {
             print_string("Console disabled\n");
             wait_enter();
         }
     } else {
-        // Not in network managed mode, prompt for username/password
         print_string("Press enter to activate console\n");
         wait_enter();
         execvp(auth[0], (char* const*)auth);
--- nic/tools/serial_console.sh
+++ nic/tools/serial_console.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
 
 CONFIG=/sysconfig/config0
-CONSOLE_CONFIG_DEPRECATED=${CONFIG}/.console
 SYSTEM_CONFIG_FILE=${CONFIG}/system-config.json
 
-enable() {
+set_config() {
     if [ ! -f $SYSTEM_CONFIG_FILE ]; then
-        jq -n --arg keyval enable '{ "console": $keyval }' > \
-            $SYSTEM_CONFIG_FILE
+        jq -n "{ \"console\": \"$1\" }" > $SYSTEM_CONFIG_FILE
     else
         # jq can't edit file in place, write to temp file and move it over. 
-        jq '. + { "console": "enable" }' $SYSTEM_CONFIG_FILE > \
+        jq ". + { \"console\": \"$1\" }" $SYSTEM_CONFIG_FILE > \
             ${SYSTEM_CONFIG_FILE}_console
         mv ${SYSTEM_CONFIG_FILE}_console $SYSTEM_CONFIG_FILE
     fi
@@ -19,27 +17,18 @@ enable() {
     if [ ! -z "${CONSOLE_PID}" ]; then
         kill -SIGTERM ${CONSOLE_PID}
     fi
+
+    sync
 }
 
-disable() {
-    # Remove both old way of configuration (.console file) and
-    # the current form of json key value in CONSOLE_CONFIG file.
-    rm -f ${CONSOLE_CONFIG_DEPRECATED}
-    if [ -f $SYSTEM_CONFIG_FILE ]; then
-        jq 'del(.console)' ${SYSTEM_CONFIG_FILE} > \
-            ${SYSTEM_CONFIG_FILE}_console
-        mv ${SYSTEM_CONFIG_FILE}_console $SYSTEM_CONFIG_FILE
-    fi
-    
-    echo "Please log out from the console"
-}
 
 case "$1" in
     enable)
-        enable
+        set_config enabled
         ;;
     disable)
-        disable
+        set_config disabled
+        echo "Please log out from the console"
         ;;
     *)
         echo "Usage: $0 {enable|disable}"
