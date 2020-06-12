ea233184f78e7f6042dfa711a81402bc1e33aa47
Sarat Kamisetty <sarat-k@users.noreply.github.com>
2020-06-12 16:49:58 -0700

    propagating LIF admin up dscagent as dscgent is (#25315)
    enhanced to handle multiple notifications now
     - revert of https://github.com/pensando/sw/pull/23952

--- nic/apollo/api/impl/apulu/lif_impl.cc
+++ nic/apollo/api/impl/apulu/lif_impl.cc
@@ -151,7 +151,6 @@ lif_impl::set_admin_state(lif_state_t state) {
 
     // update the admin state
     admin_state_ = state;
-#if 0
     // and notify lif update
     if ((type_ == sdk::platform::LIF_TYPE_HOST_MGMT) ||
         (type_ == sdk::platform::LIF_TYPE_HOST)) {
@@ -160,7 +159,6 @@ lif_impl::set_admin_state(lif_state_t state) {
         pds_lif_to_lif_status(&event.lif_info.status, this);
         g_pds_state.event_notify(&event);
     }
-#endif
 }
 
 #define nacl_redirect_action    action_u.nacl_nacl_redirect
