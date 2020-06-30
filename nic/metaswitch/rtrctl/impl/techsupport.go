ee193ed8b3c4e70b24e3dd931600fb841a226f2d
rathorea <32275334+rathorea@users.noreply.github.com>
2020-06-30 07:21:32 -0700

    tech-support code cleanup (#26236)

--- nic/metaswitch/rtrctl/impl/techsupport.go
+++ nic/metaswitch/rtrctl/impl/techsupport.go
@@ -6,6 +6,7 @@ package impl
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -21,52 +22,29 @@ var tsShowCmd = &cobra.Command{
 func init() {
 }
 
+var list []*cobra.Command
+
+func genTechSupport(cmd *cobra.Command) {
+	if list == nil {
+		list = make([]*cobra.Command, 0)
+	}
+	if cmd.RunE != nil {
+		list = append(list, cmd)
+	}
+	for _, x := range cmd.Commands() {
+		genTechSupport(x)
+	}
+}
+
 func tsShowCmdHandler(cmd *cobra.Command, args []string) {
 	fmt.Printf("\n=== Capturing rtrctl techsupport information ===\n")
 	cmd.Flags().Set("json", "false")
 	cmd.Flags().Set("detail", "true")
-	fmt.Printf("\n=== BGP related information ===\n")
-	bgpShowCmdHandler(cmd, nil)
-	fmt.Printf("\n=== BGP Peer information ===\n")
-	bgpPeersShowCmdHandler(cmd, nil)
-	fmt.Printf("\n=== BGP Peer AF information ===\n")
-	bgpPeersAfShowCmdHandler(cmd, nil)
-	fmt.Printf("\n=== BGP ip unicast nlri information ===\n")
-	bgpIPUnicastNlriShowCmdHandler(cmd, nil)
-	fmt.Printf("\n=== BGP l2vpn evpn nlri information ===\n")
-	bgpL2vpnEvpnNlriShowCmdHandler(cmd, nil)
-	fmt.Printf("\n=== BGP counters information ===\n")
-	bgpPfxCountersShowCmdHandler(cmd, nil)
-	fmt.Printf("\n=== BGP route-map information ===\n")
-	bgpRouteMapShowCmdHandler(cmd, nil)
-	fmt.Printf("\n=== EVPN VRF Status information ===\n")
-	evpnVrfStatusShowCmdHandler(cmd, nil)
-	fmt.Printf("\n=== EVPN VRF RT Status information ===\n")
-	evpnVrfRtStatusShowCmdHandler(cmd, nil)
-	fmt.Printf("\n=== EVPN EVI Status information ===\n")
-	evpnEviStatusShowCmdHandler(cmd, nil)
-	fmt.Printf("\n=== EVPN EVI RT Status information ===\n")
-	evpnEviRtStatusShowCmdHandler(cmd, nil)
-	fmt.Printf("\n=== EVPN BD MAC-IP Status information ===\n")
-	evpnBdMacIPShowCmdHandler(cmd, nil)
-	fmt.Printf("\n=== EVPN BD Status information ===\n")
-	evpnBdStatusShowCmdHandler(cmd, nil)
-	fmt.Printf("\n=== EVPN BD Interface Status information ===\n")
-	evpnBdIntfShowCmdHandler(cmd, nil)
-	fmt.Printf("\n=== software interface information ===\n")
-	routingSwIntfShowCmdHandler(cmd, nil)
-	fmt.Printf("\n=== software interface status information ===\n")
-	routingIntfStatusShowCmdHandler(cmd, nil)
-	fmt.Printf("\n=== routing static-table information ===\n")
-	staticTableShowCmdHandler(cmd, nil)
-	fmt.Printf("\n=== active routing route-table information ===\n")
-	activeRouteTableShowCmdHandler(cmd, nil)
-	fmt.Printf("\n=== interface address information ===\n")
-	routingIntfAddrShowCmdHandler(cmd, nil)
-	fmt.Printf("\n=== routing route-table information ===\n")
-	routeTableShowCmdHandler(cmd, nil)
-	fmt.Printf("\n=== vrf information ===\n")
-	routingVrfStatusShowCmdHandler(cmd, nil)
-	fmt.Printf("\n=== routing redistribution ===\n")
-	redistTableShowCmdHandler(cmd, nil)
+	genTechSupport(cmd.Root())
+	for _, myCmd := range list {
+		if strings.Contains(strings.ToLower(myCmd.Short), strings.ToLower("show")) {
+			fmt.Printf("\n=== " + myCmd.Short + " ===\n")
+			myCmd.RunE(cmd, nil)
+		}
+	}
 }
