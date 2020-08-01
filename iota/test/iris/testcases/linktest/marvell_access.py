84b4d7e0c094a0c2b101a5a5bdc83f6ebe2fa5cb
akoradha <32655671+akoradha@users.noreply.github.com>
2020-08-01 13:37:06 -0700

    iota for marvell access validation (#27475)

---
+++ iota/test/iris/testcases/linktest/marvell_access.py
@@ -0,0 +1,54 @@
+#! /usr/bin/python3
+
+import iota.harness.api as api
+
+def Setup(test_case):
+    api.Logger.info("Checking for Marvell access")
+    test_case.nodes = api.GetNaplesHostnames()
+    return api.types.status.SUCCESS
+
+def check_marvell_access(test_case):
+    cmd = "/nic/bin/halctl show port internal statistics | grep InBadOctets"
+    # for each node
+    for node in test_case.nodes:
+        naples_devices = api.GetDeviceNames(node)
+
+        # for each Naples on node
+        for naples_device in naples_devices:
+            req = api.Trigger_CreateExecuteCommandsRequest(serial=True)
+            api.Trigger_AddNaplesCommand(req, node, cmd, naples_device)
+            resp = api.Trigger(req)
+            if not api.Trigger_IsSuccess(resp):
+                api.Logger.error("Failed to trigger show port internal cmd on node %s naples %s" % (node, naples_device))
+                return api.types.status.FAILURE
+
+            for cmd_resp in resp.commands:
+                '''
+                sample output below:
+                InBadOctets    : 0
+                InBadOctets    : 0
+                InBadOctets    : 0
+                InBadOctets    : 0
+                InBadOctets    : 0
+                InBadOctets    : 0
+                InBadOctets    : 0
+                '''
+                for output_line in cmd_resp.stdout.splitlines():
+                    api.Logger.info("cmd_resp output: " + output_line)
+                    output_line_list = output_line.split(':')
+                    val = int(output_line_list[1].strip())
+                    api.Logger.info("val: " + str(val))
+                    if val == 0xFFFFFFFF:
+                        api.Logger.error("Failed Marvell access on node %s naples %s" % (node, naples_device))
+                        return api.types.status.FAILURE
+
+    return api.types.status.SUCCESS
+
+def Trigger(test_case):
+    return check_marvell_access(test_case)
+
+def Verify(test_case):
+    return api.types.status.SUCCESS
+
+def Teardown(test_case):
+    return api.types.status.SUCCESS
