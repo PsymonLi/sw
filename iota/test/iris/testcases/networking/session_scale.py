0ef761a757f0e2964092d7630d1eac8310a04f1e
Karthikeyan Arumugam <karthik@pensando.io>
2020-08-07 10:39:56 -0700

    * Added MemStats tool to collect memory usage statistics (#27179)
    * Added new testbundle to run needed mem leak tests
    * Added new test to check memory leak in loop test of HAL tech support command
    * Trigger HAL memory trim to reclaim free memory from heap at start and end of test to get correct mem usage stats

--- iota/test/iris/testcases/networking/session_scale.py
+++ iota/test/iris/testcases/networking/session_scale.py
@@ -9,9 +9,13 @@ import iota.test.iris.config.netagent.api as agent_api
 import iota.test.iris.testcases.security.utils as utils
 import iota.harness.infra.utils.periodic_timer as pt
 from iota.test.iris.utils import vmotion_utils
+from iota.test.utils.mem_stats import memStatsObjClient as memStatsObjClient
 
 from trex.astf.api import *
 from iota.test.iris.utils.trex_wrapper import *
+
+skip_tech_supp_trigger = False
+enable_memory_trim = False
 
 def GetProtocolDirectory(feature, proto):
     return api.GetTopologyDirectory() + "/gen/telemetry/{}/{}".format(feature, proto)
@@ -95,10 +99,9 @@ def switchPortFlap(tc):
     num_ports = 1
     interval = 2
     down_time  = 2
-    naples_nodes = api.GetNaplesHostnames()
 
-    api.Logger.info("Flapping switch port on %s ..."%naples_nodes)
-    ret = api.FlapDataPorts(naples_nodes, num_ports, down_time, flap_count, interval)
+    api.Logger.info("Flapping switch port on %s ..."%tc.nodes)
+    ret = api.FlapDataPorts(tc.nodes, num_ports, down_time, flap_count, interval)
     if ret != api.types.status.SUCCESS:
         api.Logger.error("Failed to flap the switch port")
         return ret
@@ -151,6 +154,54 @@ def configurationChangeEvent(tc):
 
     return api.types.status.SUCCESS
 
+def execHalMemTrimCmd(tc):
+    api.Logger.verbose("Running memory trim command...")
+    req = api.Trigger_CreateExecuteCommandsRequest(serial = False)
+    for n in tc.nodes:
+        api.Trigger_AddNaplesCommand(req, n, "/nic/bin/halctl debug memory --memory-trim")
+    resp = api.Trigger(req)
+    if resp:
+        for cmd in resp.commands:
+            api.PrintCommandResults(cmd)
+            if cmd.exit_code != 0:
+                api.Logger.error("Failed with exit_code: %s in hal memory trim command..."%cmd.exit_code)
+    return api.types.status.SUCCESS
+
+def collectMemStats(tc):
+    if tc.cancel or not tc.mem_leak_test:
+        api.Logger.info("Canceling collectMemStats...")
+        sys.exit(0)
+    api.Logger.info("Running collectMemStats for nodes: %s..."%(tc.nodes))
+    try:
+        memStatsObjClient.CollectMemUsageSnapshot(log=True)
+    except Exception as e:
+        traceback.print_exc()
+        api.Logger.error("Exception in CollectMemUsageSnapshot for nodes: %s : %s"%(tc.nodes, e))
+        return api.types.status.FAILURE
+    return api.types.status.SUCCESS
+
+def triggerTechSupport(tc):
+    if tc.cancel:
+        api.Logger.info("Canceling triggerTechSupport...")
+        sys.exit(0)
+    if skip_tech_supp_trigger:
+        return api.types.status.SUCCESS
+    api.Logger.info("Running triggerTechSupport...")
+    req = api.Trigger_CreateExecuteCommandsRequest(serial = False)
+    for n in tc.nodes:
+        api.Trigger_AddNaplesCommand(req, n, "/nic/bin/halctl show techsupport", timeout=tc.bg_trigger_cmd_timeout)
+    resp = api.Trigger(req)
+    result = api.types.status.SUCCESS
+    if resp:
+        for cmd in resp.commands:
+            # code 130 is for command terminated by CTRL+C due to timeout.
+            if cmd.exit_code != 0 and cmd.exit_code != 130:
+                api.Logger.info("Failed in triggerTechSupport. exit_code : %s..."%cmd.exit_code)
+                api.PrintCommandResults(cmd)
+                result = api.types.status.FAILURE
+    api.Logger.info("Done with triggerTechSupport...")
+    return result
+
 def connectTrex(tc):
     for w, peerList in tc.workloadPeers.items():
         api.Logger.info("%s <--> %s"%(printWorkloadInfo(w), printPeerList(peerList)))
@@ -187,9 +238,21 @@ def Setup(tc):
     tc.cancel = False
     tc.workloadPeers = {}
     tc.events = []
+    tc.nodes = api.GetNaplesHostnames()
+    tc.mem_leak_test = getattr(tc.args, "mem_leak_test", False)
+    tc.mem_incr_threshold = getattr(tc.args, "mem_incr_threshold", 1)
+    tc.bg_trigger_cmd_timeout = 200
+
+    if enable_memory_trim:
+        # Reclaim all free memory before start & end of test
+        # to find the amount of memory leaked after the test
+        execHalMemTrimCmd(tc)
 
     try:
         findWorkloadPeers(tc)
+
+        if tc.mem_leak_test:
+            memStatsObjClient.InitNodesForMemUsageStats()
 
         ret = connectTrex(tc)
         if ret != api.types.status.SUCCESS:
@@ -238,6 +301,7 @@ def verifySessions(tc):
 
 def Verify(tc):
 
+    result = api.types.status.SUCCESS
     if getattr(tc.args, 'vmotion_enable', False):
         vmotion_utils.PrepareWorkloadRestore(tc)
 
@@ -250,19 +314,37 @@ def Verify(tc):
         if ret != api.types.status.SUCCESS:
             return ret
         api.Logger.info("Verified session stats successfully.")
-        
+
+        if tc.mem_leak_test:
+            api.Logger.info("Waiting for 300 secs to allow system to reclaim "
+                            "free memory from heap before collecting final MemStats")
+            # wait for 300 secs for memory to be freed to heap
+            time.sleep(300)
+            # Reclaim all free memory from heap at end of test
+            if enable_memory_trim:
+                execHalMemTrimCmd(tc)
+
+            memStatsObjClient.CollectMemUsageSnapshot(log=True)
+            memStatsObjClient.PrintMemUsageHistory()
+            if memStatsObjClient.CheckMemLeakFromHistory(pname='hal', acceptable_increase=tc.mem_incr_threshold):
+                api.Logger.error("MemLeak Found...")
+                result = api.types.status.FAILURE
+            else:
+                api.Logger.info("NO MemLeak Found in HAL process...")
+   
         ret = utils.isHalAlive()
         if ret != api.types.status.SUCCESS:
             api.Logger.error("HAL is DEAD ..")
-            return ret
-        api.Logger.info("Verified the HAL process.")
+            result = api.types.status.FAILURE
+        else:
+            api.Logger.info("Verified the HAL process.")
 
     except Exception as e:
         traceback.print_exc()
         api.Logger.error("Failed to verify : %s"%(e))
         return api.types.status.FAILURE
 
-    return api.types.status.SUCCESS
+    return result
 
 def cleanup(tc):
     for w in tc.workloadPeers.keys():
