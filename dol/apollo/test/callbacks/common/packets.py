afe9a75ff3db6363a99a3f345ee6938fe5669ea3
LoganathanNallusamy <loganan@pensando.io>
2020-08-18 08:15:25 +0530

    Bump In The Wire(BITw) negative cases (#27864)

--- dol/apollo/test/callbacks/common/packets.py
+++ dol/apollo/test/callbacks/common/packets.py
@@ -6,3 +6,6 @@ import apollo.config.topo as topo
 
 def AnyUplinkPort(tc, args=None):
     return [topo.Ports.UPLINK_0, topo.Ports.UPLINK_1]
+
+def GetPacketFromMlist(tc):
+    return tc.packets.Get(tc.module.args.input_pkt)
