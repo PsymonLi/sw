8e2cecd76dc315f334ebbe3cefe2cbe162bb5d61
JINGYI ZHANG <41308176+jingyizPensando@users.noreply.github.com>
2020-08-21 13:44:01 -0700

    Not include mgmt interface to ESXi (#28214)
    During VMware cert, there are many tests that will fail when mgmt
    interfaces are presented in the host. This file is for cert usage only.

---
+++ platform/drivers/esxi/ionic_en/ionic_devices_no_mgmt.py
@@ -0,0 +1,96 @@
+"""
+/*
+ * Copyright (c) 2020 Pensando Systems. All rights reserved.
+ */
+"""
+
+DEV = { 
+   "priority"                   : "default",
+   "bus type"                   : "pci",
+   "device info"                : { 
+      "vendor list"                : [ 
+         {   
+            "vendor id"               : "1dd8",
+            "vendor description"      : "Pensando Systems",
+            "devices" : [ 
+               {
+                  "device id"         : "1002",
+                  "sub vendor id"     : "1dd8",
+                  "sub device id"     : "4000",
+                  "sub device desc"   : "DSC-100 40/100G 2-port 8G RAM 8G eMMC G1 Services Card, Ethernet Controller"
+               },
+               {
+                  "device id"         : "1002",
+                  "sub vendor id"     : "1dd8",
+                  "sub device id"     : "4001",
+                  "sub device desc"   : "DSC-100 40/100G 2-port 4G RAM 8G eMMC G1 Services Card, Ethernet Controller"
+               },
+               {
+                  "device id"         : "1002",
+                  "sub vendor id"     : "1dd8",
+                  "sub device id"     : "4002",
+                  "sub device desc"   : "DSC-25 10/25G 2-port 4G RAM 8G eMMC G1 Services Card, Ethernet Controller"
+               },
+               {
+                  "device id"         : "1002",
+                  "sub vendor id"     : "1dd8",
+                  "sub device id"     : "4005",
+                  "sub device desc"   : "DSC-25 10/25G 2-port 8G RAM 8G eMMC G1 Services Card, Ethernet Controller"
+               },
+               {
+                  "device id"         : "1002",
+                  "sub vendor id"     : "1dd8",
+                  "sub device id"     : "4006",
+                  "sub device desc"   : "DSC-100V 40/100G 2-port 4G RAM 8G eMMC G1 Services Card, Ethernet Controller"
+               },
+               {
+                  "device id"         : "1002",
+                  "sub vendor id"     : "1dd8",
+                  "sub device id"     : "4007",
+                  "sub device desc"   : "DSC-25-OCP 10/25G 2-port 4G RAM 8G eMMC G1 Services Card, Ethernet Controller"
+               },
+               {
+                  "device id"         : "1002",
+                  "sub vendor id"     : "1dd8",
+                  "sub device id"     : "4008",
+                  "sub device desc"   : "DSP DSC-25 10/25G 2p SFP28 Card, Ethernet Controller"
+               },
+               {
+                  "device id"         : "1002",
+                  "sub vendor id"     : "1dd8",
+                  "sub device id"     : "4009",
+                  "sub device desc"   : "DSC-100V 50/100G 2-port 8G RAM 64G eMMC G1 Services Card, Ethernet Controller"
+               },
+               {
+                  "device id"         : "1002",
+                  "sub vendor id"     : "1dd8",
+                  "sub device id"     : "400a",
+                  "sub device desc"   : "DSC-100 40/100G 2-port 8G RAM 16G eMMC G1 Services Card, Ethernet Controller"
+               },
+               {
+                  "device id"         : "1002",
+                  "sub vendor id"     : "1dd8",
+                  "sub device id"     : "400b",
+                  "sub device desc"   : "DSC-25-OCP 10/25G 2-port 4G RAM 8G eMMC G1 Services Card, Ethernet Controller"
+               },
+               {
+                  "device id"         : "1002",
+                  "sub vendor id"     : "1dd8",
+                  "sub device id"     : "400c",
+                  "sub device desc"   : "DSC-25 10/25G 2-port 4G RAM 8G eMMC G1 Services Card, Ethernet Controller"
+               },
+               {
+                  "device id"         : "1002",
+                  "sub vendor id"     : "1dd8",
+                  "sub device id"     : "400d",
+                  "sub device desc"   : "Pensando DSP DSC-100 100G 2p QSFP28 Card, Ethernet Controller"
+               },
+               {
+                  "device id"         : "1003",
+                  "description"       : "Pensando Ethernet VF",
+               },
+            ],
+         },
+      ]
+   },
+}
